78026479bf8775559002ef1d2ebae456aa2ff0f0
MORTREUX Vincent <mortreux.vincent@gmail.com>
2023-07-11 13:50:09 +0200

    Fleche droite et changement image

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,24 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+const arrowRight = document.querySelector(".arrow_right");
+const currentBanner = document.querySelector(".banner-img");
+
+const currentPath = './assets/images/slideshow/';
+let cpt =0;
+
+const init = ()=> {
+	currentBanner.src= currentPath +slides[cpt].image;
+}
+arrowRight.addEventListener('click',()=>{
+	if(cpt == slides.length - 1){ // lorsqu'on est sur la dernière diapositive
+		cpt = 0; // retour 1er diapositive
+	}else{
+		cpt++;	
+	}	
+	currentBanner.src= currentPath + slides[cpt].image;
+    console.log("flèche de droite");
+});
+
+init();
