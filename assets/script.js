3581835fbef65c8aaae0e8e35194bc6c94f0cb74
MORTREUX Vincent <mortreux.vincent@gmail.com>
2023-07-12 17:46:06 +0200

    Ajout du texte pour l'image

--- assets/script.js
+++ assets/script.js
@@ -19,6 +19,7 @@ const slides = [
 const arrowRight = document.querySelector(".arrow_right");
 const arrowLeft = document.querySelector(".arrow_left");
 const currentBanner = document.querySelector(".banner-img");
+const currentTexte = document.querySelector(".texte");
 const dots = document.querySelector(".dots");
 
 const currentPath = './assets/images/slideshow/';//chemin d'accès des images
@@ -26,7 +27,7 @@ let cpt = 0;// Initialiser compteur à 0
 
 const init = () => { // Fonction pour afficher la première diapositive
   currentBanner.src = currentPath + slides[cpt].image;
-};
+  currentTexte.innerHTML = slides[cpt].tagLine;
 
 for(let i =0; i < slides.length;i++){ //Pour chaque elements
     const span = document.createElement('span'); //Crée balise span
@@ -34,7 +35,9 @@ for(let i =0; i < slides.length;i++){ //Pour chaque elements
     dots.appendChild(span); //envoie parents
     }
     activeDot()
+
 };
+
 const activeDot = () => {
     const tabDots = document.querySelectorAll(".dot"); // sélectionne tous les points
     for (let i = 0; i < tabDots.length; i++) { 
@@ -43,29 +46,30 @@ const activeDot = () => {
     }
     tabDots[cpt].classList = "dot dot_selected"; // ajoute "dot_selected"
 }
-
 arrowRight.addEventListener('click', () => {// écouteur d'événements diapositive suivante
   if (cpt == slides.length - 1) { //si on est sur la dernière diapositive
     cpt = 0; // retourner à la première diapositive
   } else {
     cpt++; // passer à la diapositive suivante
   }
-  currentBanner.src = currentPath + slides[cpt].image; // Changement image
 
+  currentBanner.src = currentPath + slides[cpt].image; // Changement image
+  currentTexte.innerHTML = slides[cpt].tagLine
   activeDot()
   console.log("flèche de droite");
 });
-
 arrowLeft.addEventListener('click', () => {
   if (cpt == 0) {// si on est sur la première diapositive
     cpt = slides.length - 1; // retourner à la dernière diapositive
   } else {
     cpt--; 
   }
-  currentBanner.src = currentPath + slides[cpt].image;
 
+  currentBanner.src = currentPath + slides[cpt].image;
+  currentTexte.innerHTML = slides[cpt].tagLine
   activeDot()
   console.log("flèche de gauche");
 });
+
 
 init();
